8202256fa2933797f6282acc0b36f9038eb16f2e
Himadri <hvyas738@gmail.com>
2019-11-05 16:31:08 -0500

    modal first with javascript completed just two modal left

--- Scripts/Zuc/js/srs-interactivity.js
+++ Scripts/Zuc/js/srs-interactivity.js
@@ -7,5 +7,16 @@ $('.addPlanButton').click(function(){
 })
 
 
+function openModal(className,form){
+    $(className.data.key).removeClass('non-visible');
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  }
+    function closeModal(className){
+    $(className.data.key).addClass('non-visible');
+  }
+
+$('.addItemModal').bind('click',{key:'.addItem-modal'},openModal);
+let cross = $('.addItem-form').find('.purple-background h2');
+cross.bind('click',{key:'.addItem-modal'},closeModal);
 
 });

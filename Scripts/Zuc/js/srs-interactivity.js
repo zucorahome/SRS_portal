e1e60d90fe0ff08f81396a584c324da907410187
Himadri <hvyas738@gmail.com>
2019-11-10 22:31:31 -0500

    retailer profile completed

--- Scripts/Zuc/js/srs-interactivity.js
+++ Scripts/Zuc/js/srs-interactivity.js
@@ -28,20 +28,31 @@ $('.addItem-save').click(function(){
 
 });
 
+// Plan registration add plan function
+
 $('.planRegis-mainSubmit-button').bind('click',{key:'.mainSubmit-modal-container'},openModal);
 
 $('.mainSubmit-modal').find('.close-modal').bind('click',{key:'.mainSubmit-modal-container'},closeModal);
 
+//For removing plan registrtation form details and closing the modal
 $('.remove-planRegis').bind('click',{key:'.remove-planModal-container'},openModal);
 
 $('.remove-planModal').find('.close-modal').bind('click',{key:'.remove-planModal-container'},closeModal);
 
-$('.contact-form-button').bind('click',{key:'.shipping-locations-modal'},openModal);
+// Retailer Profile screen function for opening shipping location modal and closing
+$('.location-form-button').bind('click',{key:'.shipping-locations-modal'},openModal);
+//editing the shiiping location data. the form should populate with entered data
+$('.shipping-location-edit').bind('click',{key:'.shipping-locations-modal'},openModal);
 
 let shippingCross = $('.shipping-locations-form').find('.purple-background h2');
 
 shippingCross.bind('click',{key:'.shipping-locations-modal'},closeModal);
 
+//Contact form  
 
+$('.contact-form-button').bind('click',{key:'.contacts-form-modal'},openModal);
+let contactFormClose = $('.contacts-form').find('.purple-background h2');
+
+contactFormClose.bind('click',{key:'.contacts-form-modal'},closeModal);
 
 });
